3de89a614246b9a6df646f361380a5ce4b481cf3
Percy-Coding <55159839+Percy-Coding@users.noreply.github.com>
2021-04-28 02:15:48 -0500

    v1.0 mockdata

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,5 +1,14 @@
 const checkbox = document.getElementById('checkbox');
+const darkModeElements = document.getElementsByClassName('dm');
 
 checkbox.addEventListener('change',()=>{
    document.body.classList.toggle('dark'); 
+   Array.from(darkModeElements).forEach((elem) => {
+      elem.classList.toggle('dark');
+   });
 });
+
+
+function onClickJoin(){
+   alert('Modo de Prueba Causita');
+}
